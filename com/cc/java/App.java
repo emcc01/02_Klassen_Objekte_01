e3c5ce04fc946c69e4d0a36a27a80a1fa08d99a4
Eva <e.moritz.comcave@gmail.com>
2023-05-22 11:05:56 +0200

    2 klassen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,7 +2,24 @@ package com.cc.java;
 
 public class App {
     
-    
+    public static void main(String[] args) {
+
+        /* Instanziierung Klasse --> Objekt */
+        // Cat -- Referenztyp
+        // cat -- Referenzvariable
+
+
+        Cat cat1 = new Cat();
+        System.out.println(cat1);
+        cat1.tellYourAddress();
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
+     
+     
+        
+    }
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,9 @@
+package com.cc.java;
+
+public class Cat {
+
+    public void tellYourAddress() {
+        System.out.println("My address: " + this);
+        // this = Instanzvariable, Instanz = Objekt
+    }
+}
